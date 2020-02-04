3a7d8e66b66fd4bf659523e0671636e3fd7a4690
AaronFeng753 <52553688+AaronFeng753@users.noreply.github.com>
2020-02-04 16:00:05 +0800

    Add files via upload

--- SRC/Waifu2x-Extension-GUI-Start.py
+++ SRC/Waifu2x-Extension-GUI-Start.py
@@ -7,6 +7,28 @@ import psutil
 import json
 import ctypes
 
+def remove_safe(path_):
+	if os.path.exists(path_):
+		os.remove(path_)
+
+def AdminTest():
+	current_dir = os.path.dirname(os.path.abspath(__file__))
+	tmp_dir = current_dir+'\\'+'admintest.tmp'
+	if os.path.exists(tmp_dir) == False:
+		try:
+			with open(tmp_dir,'w+') as f:
+				f.write('0100000101100001011100100110111101101110')
+		except BaseException:
+			return False
+		if os.path.exists(tmp_dir) == False:
+			return False
+		else:
+			remove_safe(tmp_dir)
+			return True
+	else:
+		remove_safe(tmp_dir)
+		return True
+
 def judgeprocess(processname):
     pl = psutil.pids()
     for pid in pl:
@@ -24,8 +46,12 @@ os.system('cls')
 print(' =============')
 print(' |Starting...|')
 print(' =============')
-os.chdir('waifu2x-extension-gui\\')
-os.system('start Waifu2x-Extension-GUI.exe')
-while judgeprocess('Waifu2x-Extension-GUI.exe'):
-	time.sleep(0.01)
-time.sleep(0.5)
+
+if AdminTest():
+	os.chdir('waifu2x-extension-gui\\')
+	os.system('start Waifu2x-Extension-GUI.exe')
+	while judgeprocess('Waifu2x-Extension-GUI.exe'):
+		time.sleep(0.01)
+	time.sleep(0.5)
+else:
+	ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, __file__, None, 1)
