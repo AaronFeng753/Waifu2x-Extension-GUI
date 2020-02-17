0a2d8616a4165d385e20554fa02aad33ebe9e9fa
AaronFeng753 <52553688+AaronFeng753@users.noreply.github.com>
2020-02-17 11:05:31 +0800

    Delete ForceStop_waifu2xEX.py

--- SRC/ForceStop_waifu2xEX.py
+++
@@ -1,8 +0,0 @@
-import os
-try:
-	os.system('taskkill /f /t /fi "imagename eq Waifu2x-Extension-GUI.exe"')
-	os.system('taskkill /f /t /fi "imagename eq python_ext_Waifu2xEX.exe"')
-	os.system('taskkill /f /t /fi "imagename eq Waifu2x-Extension-GUI-Start.exe"')
-	
-except BaseException:
-	print('Failed')
